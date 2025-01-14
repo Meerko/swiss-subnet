e2831710869a4e675555276e07d7e5303cb8a4f2
Mirko Maggiore <81267022+Meerko@users.noreply.github.com>
2025-01-14 16:58:50 +0100

    Create juno.config.mjs

---
+++ juno.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from "@junobuild/config";
+
+export default defineConfig({
+  satellite: {
+    id: "gwank-kqaaa-aaaal-ar2cq-cai", // Replace with your satellite ID
+    source: "build" // Replace with your build output directory
+  }
+});
