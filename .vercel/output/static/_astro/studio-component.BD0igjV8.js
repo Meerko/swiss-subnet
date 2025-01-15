a2c37823e425a8ea62ecd28a1026f0500b562684
Mirko Maggiore <mirkomaggiore@icloud.com>
2025-01-15 01:44:06 +0100

    Adapter

---
+++ .vercel/output/static/_astro/studio-component.BD0igjV8.js
@@ -0,0 +1,1 @@
+import{as as p}from"./studio-component.CJq9QXFT.js";import"./client.Dm-eYAro.js";export{p as StudioComponent};
--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,5 +19,7 @@ export default defineConfig({
       studioBasePath: "/studio",
     }),
     react(),
-  ]
+  ],
+  output: 'static',
+  adapter: vercel(),
 });
