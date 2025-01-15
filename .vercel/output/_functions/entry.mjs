35e4b5b9776033bfa6dfde1f101f356091d1afb6
Giorgia <giorgiabagnasco@gmail.com>
2025-01-15 08:58:46 +0000

    fixes

---
+++ .vercel/output/_functions/entry.mjs
@@ -0,0 +1,31 @@
+import { renderers } from './renderers.mjs';
+import { c as createExports } from './chunks/entrypoint_rYiBHPB2.mjs';
+import { manifest } from './manifest_BZXefRKX.mjs';
+
+const serverIslandMap = new Map();;
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/api/contact.astro.mjs');
+const _page2 = () => import('./pages/studio/_---params_.astro.mjs');
+const _page3 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/api/contact.ts", _page1],
+    ["node_modules/@sanity/astro/dist/studio/studio-route.astro", _page2],
+    ["src/pages/index.astro", _page3]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "93dd9946-51a5-4594-94f3-1bdd661429ab",
+    "skewProtection": false
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
