42de0661e58c5910e796860569eb4a0bbe95c56a
Mirko Maggiore <mirkomaggiore@icloud.com>
2025-01-15 09:32:21 +0100

    Up

--- .vercel/output/static/_astro/studio-component.BD0igjV8.js
+++
@@ -1,1 +0,0 @@
-import{as as p}from"./studio-component.CJq9QXFT.js";import"./client.Dm-eYAro.js";export{p as StudioComponent};
--- astro.config.mjs
+++ astro.config.mjs
@@ -21,5 +21,4 @@ export default defineConfig({
     react(),
   ],
   adapter: vercel(),
-  output: "static",
 });
